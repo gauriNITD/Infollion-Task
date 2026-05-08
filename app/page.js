93f7d4470eaa5b163fdaf6a2a999a43b3a501350
Gauri Agarwal <231210045@nitdelhi.ac.in>
2026-05-09 00:57:24 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import ChatContainer from "@/components/Chat/ChatContainer";
+
+export default function Home() {
+  return (
+    <main className="h-screen overflow-hidden bg-[#09090b]">
+      <ChatContainer />
+    </main>
+  );
+}
